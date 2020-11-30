869155f1ca86df9e7d1dc342fd5d87317a3e9c59
jun097kim <jun097kim@gmail.com>
2020-11-30 22:54:41 +0900

    week3

---
+++ src/main/kotlin/kim/jun0/ch04/4.1.2_1_OpenFinalAbstractModifiers.kt
@@ -0,0 +1,12 @@
+package kim.jun0.ch04.ex1_2_1_OpenFinalAbstractModifiers
+
+interface Clickable {
+    fun click()
+    fun showOff() = println("I'm clickable!")
+}
+
+open class RichButton: Clickable {
+    fun disable() {}
+    open fun animate() {}
+    override fun click() {}
+}
---
+++ src/main/kotlin/kim/jun0/ch04/4.1.2_2_OpenFinalAbstractModifiers1.kt
@@ -0,0 +1,10 @@
+package kim.jun0.ch04.ex1_2_2_OpenFinalAbstractModifiers1
+
+interface Clickable {
+    fun click()
+    fun showOff() = println("I'm clickable!")
+}
+
+open class RichButton: Clickable {
+    final override fun click() {}
+}
